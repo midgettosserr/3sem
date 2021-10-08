6088339aeac6572c10d27cea7091529405368529
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-10-09 00:33:18 +0300

    main

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,73 +3,60 @@
 
 using namespace std;
 
-void Menu() {
-    cout <<
-        "1 - Print the length of the nephroid\n"
-        "2 - Print the radius of nephroid\n"
-        "3 - Print area\n"
-        "4 - Print the x and y values\n"
-        "5 - Print the equation of the nephroid\n"
-        "6 - Print the radius of nephroid\n"
-        //"7 - Change the radius\n"
-        "8 - Exit\n";
-}
-
 int main() {
-    int c = 1;
-    cout << "Create nephroid\n";
-    nephroid n;
-    double rad;
-
-    cout << "Enter the radius: ";
-    cin >> rad;
-    n.radius(rad);
-
-    Menu();
-    while (c != 8) {
-        cout << "Enter the menu selection item: ";
-        cin >> c;
-        switch (c) {
-        case 1: {
-            double l = n.get_len();
-            cout << "The the length of the curve: " << l << "\n";
-            break;
+    nephroid neph;
+    int menu;
+    double a, t;
+    char str[255];
+    do {
+        cout << "1 - Set radius of nephroid" << endl;
+        cout << "2 - Get radius of nephroid" << endl;
+        cout << "3 - Get arclength" << endl;
+        cout << "4 - Get area" << endl;
+        cout << "5 - Get radius of curvature" << endl;
+        cout << "6 - Get x and y coordinates" << endl;
+        cout << "7 - Get the equation of implicit representation" << endl;
+        cout << "0 - Quit" << endl;
+        cout << "> ";
+        cin >> menu;
+        switch (menu) {
+            case 1:
+                cout << "Input radius a = ";
+                cin >> a;
+                try {
+                    neph.set_radius(a);
+                } catch (invalid_argument ex) {
+                    cout << "Error! " << ex.what() << endl;
+                }
+                break;
+            case 2:
+                cout << "Radius a = " << neph.get_radius() << endl;
+                break;
+            case 3:
+                cout << "Arclength L = " << neph.get_len() << endl;
+                break;
+            case 4:
+                cout << "Area A = " << neph.get_area() << endl;
+                break;
+            case 5:
+                cout << "Enter t = ";
+                cin >> t;
+                cout << "Radius of curvature p = " << neph.get_curvature(t) << endl;
+                break;
+            case 6:
+                cout << "Enter t = ";
+                cin >> t;
+                cout << "Coordinates are (" << neph.get_x(t) << ", " << neph.get_y(t) << ")" << endl;
+                break;
+            case 7:
+                neph.get_equation(str);
+                cout << str << endl;
+                break;
+            case 0:
+                break;
+            default:
+                cout << "Incorrect menu item" << endl;
+                break;
         }
-        case 2: {
-            cout << "Enter the value of the parameter t: ";
-            double t;
-            cin >> t;
-            double r = n.get_curvature(t);
-            cout << "The radius of curvature: " << r << "\n";
-            break;
-        }
-        case 3: {
-            double s = n.get_s();
-            cout << "Area: " << s << "\n";
-            break;
-        }
-        case 4: {
-            cout << "Enter the value of the parameter t: ";
-            double t;
-            cin >> t;
-            double x = n.get_x(t);
-            double y = n.get_y(t);
-            cout << "X value: " << x << "\n";
-            cout << "Y value: " << y << "\n";
-            break;
-        }
-        case 5: {
-            string str = n.get_equation();
-            cout << str << "\n";
-            break;
-        }
-        case 6: {
-            double f = n.get_radius();
-            cout << "Radius: " << f << "\n";
-            break;
-        }
-        case 7: {
-        }
-        }
-    }
+    } while (menu != 0);
 }
