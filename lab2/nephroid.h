f76de93e5c19099608520684605061b27edabeea
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-10-08 22:39:29 +0300

    library

--- lab2/nephroid.h
+++ lab2/nephroid.h
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
@@ -7,20 +6,17 @@ class nephroid {
   private:
     double r;
   public:
-    nephroid() {
-        r = 0;
-    }
-    nephroid(double a) {
-        if (a < 0)
-            //cout << "Error! Please repeat the input: ";
-        r = a;
-    }
-    void radius(double);
-    double get_len() const;
-    double get_s() const;
-    double get_curvature(double) const;
+    nephroid();
+    nephroid(double a);
+    nephroid(const nephroid&);
+
+    void set_radius(double);
     double get_radius() const;
+
+    double get_len() const;
+    double get_area() const;
+    double get_curvature(double) const;
     double get_x(double) const;
     double get_y(double) const;
-    std::string get_equation() const;
+    void get_equation(char[]) const;
 };
