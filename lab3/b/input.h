6f112da013218f42b4f8b31f8c7df3a175ce0d46
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-10-23 12:57:52 +0300

    Create input.h

---
+++ lab3/b/input.h
@@ -0,0 +1,18 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <iostream>
+#include "table.h"
+
+#include <stdexcept>
+#include <limits>
+
+template <class T>
+void getValue(T &a);
+
+Table getTable();
+
+void printMenu();
+void menu(Table&, int);
+
+#endif
