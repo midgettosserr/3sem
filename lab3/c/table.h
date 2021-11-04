1a0e167362f72833ce05b60bcca819c970d2e657
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-11-04 16:03:04 +0300

    table.h

--- lab3/c/table.h
+++ lab3/c/table.h
@@ -18,6 +18,8 @@ struct Element {
     
     Element(const Element& element);
     Element& operator=(const Element& element);
+    Element(Element&& element);
+    Element& operator=(Element&& element);
     ~Element();
 };
 
@@ -39,10 +41,12 @@ public:
     Table& operator+=(const Element& element);
     Table& operator-=(int key);
     const char* operator[](int key);
-    Table operator+(const Table& table) const;
+    friend Table operator+(const Table& table1, const Table& table2); 
     
     Table(const Table& table);
     Table& operator=(const Table& table);
+    Table(Table&& table);
+    Table& operator=(Table&& table);
     ~Table();
 };
 
