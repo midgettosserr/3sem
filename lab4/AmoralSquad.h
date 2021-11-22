8647b1a9e615b942affedbb52672f229e6a49052
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-11-22 22:24:21 +0300

    another base squad

--- lab4/AmoralSquad.h
+++ lab4/AmoralSquad.h
@@ -16,28 +16,28 @@ protected:
 	int count;
 	int experience;	//опыт за уничтожение отряда
 public:
-    AmoralSquad(std::string creatureName = "", const School& school = School(), int initiative = 0, int speed = 0, int damage = 0, int protection = 0, int count = 0, int experience = 0);
-    virtual ~AmoralSquad() {}
+	AmoralSquad(std::string creatureName = "", const School& school = School(), int initiative = 0, int speed = 0, int damage = 0, int protection = 0, int count = 0, int experience = 0);
+	virtual ~AmoralSquad() {}
  
-    virtual std::string getCreatureName() const {return creatureName;}
-    virtual School &getSchool() {return school;}
-    virtual int getInitiative() const {return initiative;}
-    virtual int getSpeed() const {return speed;}
-    virtual int getDamage() const {return damage;}
-    virtual int getProtection() const {return protection;}
-    virtual int getCount() const {return count;}
-    virtual int getExperience() const {return experience;}
+	virtual std::string getCreatureName() const {return creatureName;}
+    	virtual School &getSchool() {return school;}
+    	virtual int getInitiative() const {return initiative;}
+    	virtual int getSpeed() const {return speed;}
+    	virtual int getDamage() const {return damage;}
+    	virtual int getProtection() const {return protection;}
+    	virtual int getCount() const {return count;}
+    	virtual int getExperience() const {return experience;}
  
-    virtual void setCreatureName(std::string name);
-    virtual void setSchool(const School &school);
-    virtual void setInitiative(int initiative);
-    virtual void setSpeed(int speed);
-    virtual void setDamage(int damage);
-    virtual void setProtection(int protection);
-    virtual void setCount(int count);
-    virtual void setExperience(int experience);
+    	virtual void setCreatureName(std::string name);
+    	virtual void setSchool(const School &school);
+    	virtual void setInitiative(int initiative);
+    	virtual void setSpeed(int speed);
+    	virtual void setDamage(int damage);
+    	virtual void setProtection(int protection);
+    	virtual void setCount(int count);
+    	virtual void setExperience(int experience);
     
-	//получение и нанесение урона
+    	//получение и нанесение урона
 	virtual void makeDamageTo(int damage);
 	virtual int getDamageFrom() const;
 };
