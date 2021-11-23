e620cc6ed7b8b5b1e6065091383a237c6f57909c
midgettosserr <71627318+midgettosserr@users.noreply.github.com>
2021-11-23 12:31:23 +0300

    another part of cpp

---
+++ lab4/AmoralSquad.cpp
@@ -0,0 +1,76 @@
+#include "AmoralSquad.h"
+
+AmoralSquad::AmoralSquad(std::string creatureName, const School& school, int initiative,
+             int speed, int damage, int protection, int count, int experience) :
+            creatureName(creatureName), school(school), initiative(initiative), speed(speed),
+            damage(damage), protection(protection), count(count), experience(experience) {
+    
+}
+
+void AmoralSquad::setCreatureName(std::string name) {
+	this->creatureName = name;
+}
+
+void AmoralSquad::setSchool(const School &school) {
+	this->school = school;
+}
+
+void AmoralSquad::setInitiative(int initiative) {
+	if (initiative < 0) {
+		throw std::invalid_argument ("initiative < 0");
+	}
+	this->initiative = initiative;
+}
+
+void AmoralSquad::setSpeed(int speed) {
+	if (speed < 0) {
+		throw std::invalid_argument("speed < 0");
+	}
+	this->speed = speed;
+}
+
+void AmoralSquad::setDamage(int damage) {
+	if (damage < 0) {
+		throw std::invalid_argument("damage < 0");
+	}
+	this->damage = damage;
+}
+
+void AmoralSquad::setProtection(int protection) {
+	if (protection < 0) {
+		throw std::invalid_argument("protection < 0");
+	}
+	this->protection = protection;
+}
+
+void AmoralSquad::setCount(int count) {
+	if (count < 0) {
+		throw std::invalid_argument("count < 0");
+	}
+	this->count = count;
+}
+
+void AmoralSquad::setExperience(int experience) {
+	if (experience < 0) {
+		throw std::invalid_argument("experience < 0");
+	}
+	this->experience = experience;
+}
+    
+//получение и нанесение урона
+void AmoralSquad::makeDamageTo(int damage) {
+	protection -= damage;
+	if (protection <= 0) {
+		if (count > 0)
+			count--;
+		if (count == 0)
+			protection = 0;
+		else
+			protection = 100;
+	}
+
+}
+
+int AmoralSquad::getDamageFrom() const {
+	return getDamage();
+}
